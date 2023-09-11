6e1d46cde8adc40efa2125dd22056a2c406acd68
Twisside <ana.maria.timciuc.01@gmail.com>
2023-09-11 18:49:16 +0300

    first commit

---
+++ Chllng_3.cs
@@ -0,0 +1,34 @@
+﻿
+
+namespace Class_exampl;
+// make
+//  *  
+// *** 
+//*****
+// *** 
+//  *  
+
+public class Chllng_3
+{
+    static int size = 0;
+
+    public static void diamond() //pain
+    {
+        for (int i = 0; i < Chllng_3.size; i++)
+        {
+            for (int j = 0; j < Chllng_3.size; j++)
+            {
+                if (j < Chllng_3.size / 2)
+                {
+                    Console.Write(' ');
+                }
+                
+                if 
+
+
+            }
+            
+        }
+    }
+
+}
---
+++ Cs stuffff/Carrrr.cs
@@ -0,0 +1,26 @@
+﻿namespace Cs_stuffff
+{
+public class Carrrr
+{
+    private static string brand = "mazda";
+    private static string model = "mx-5";
+    private static int year = 1993;
+    private static string colour = "red";
+
+    private string licence_plate = "mzdfl";
+    private int hp = 180;
+    private int milliadge = 15000;
+    private int transmission = 0; // 0 - mechanical, 1 - automatic
+    private static float speed = 0;
+
+    public void Details()
+    {
+        Console.WriteLine(Carrrr.brand, Carrrr.model, Carrrr.year, Carrrr.colour);
+    }
+
+    public void Motion(float speed)
+    {
+        Carrrr.speed += Convert.ToSingle(Console.ReadLine());
+    }
+}
+}
