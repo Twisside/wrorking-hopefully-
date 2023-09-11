183abb4d0a27b1d648fa66853b0fda3adb395bae
Twisside <136237170+Twisside@users.noreply.github.com>
2023-09-11 21:00:24 +0300

    Car.cs

--- Car.cs
+++ Car.cs
@@ -1,2 +1,34 @@
-sorry, i can't figure out how to remove this file from ﻿this repository
-the class for the lab is in folder 'Cs stuffff' under the name 'Carrrr.cs'
+//if this doesn't work (and it probably won't) the original file is .../Cs stuffff/Carrrr.cs
+
+namespace Cs_stuffff
+{
+public class Carrrr
+{
+    private static string colour = "red";
+    private static string state = "stationary"
+    private static float speed = 0;
+
+    public void Details()
+    {
+        Console.WriteLine(Carrrr.colour, Carrrr.state, Carrrr.speed);
+    }
+
+    public void Motion(float speed);
+    {
+        Carrrr.speed = Convert.ToSingle(Console.ReadLine());
+        if(Carrrr.speed > 0)
+        {
+            Carrrr.state = "moving";
+        }
+        if(Carrrr.speed == 0)
+        {
+            Carrrr.state = "stationary";
+        }
+        if(Carrrr.speed < 0)
+        {
+            Console.WriteLine("That's not how reverse works...");
+        }
+    }
+}
+}
+
