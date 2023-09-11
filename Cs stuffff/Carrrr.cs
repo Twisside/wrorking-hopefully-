075cb007f0452efc50ab19d7bbd406f1ec8731db
Twisside <136237170+Twisside@users.noreply.github.com>
2023-09-11 20:55:38 +0300

    Carrrr.cs

--- Cs stuffff/Carrrr.cs
+++ Cs stuffff/Carrrr.cs
@@ -24,7 +24,7 @@ public class Carrrr
         }
         if(Carrrr.speed < 0)
         {
-            Console.WriteLine("That's not how reverse works..."
+            Console.WriteLine("That's not how reverse works...");
         }
     }
 }
