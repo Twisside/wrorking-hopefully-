d0deb8a10fb784fd4a6898ea100e389f1aa1b156
Twisside <136237170+Twisside@users.noreply.github.com>
2023-09-11 20:55:03 +0300

    Carrrr.cs

--- Cs stuffff/Carrrr.cs
+++ Cs stuffff/Carrrr.cs
@@ -2,25 +2,30 @@
 {
 public class Carrrr
 {
-    private static string brand = "mazda";
-    private static string model = "mx-5";
-    private static int year = 1993;
     private static string colour = "red";
-
-    private string licence_plate = "mzdfl";
-    private int hp = 180;
-    private int milliadge = 15000;
-    private int transmission = 0; // 0 - mechanical, 1 - automatic
+    private static string state = "stationary"
     private static float speed = 0;
 
     public void Details()
     {
-        Console.WriteLine(Carrrr.brand, Carrrr.model, Carrrr.year, Carrrr.colour);
+        Console.WriteLine(Carrrr.colour, Carrrr.state, Carrrr.speed);
     }
 
-    public void Motion(float speed)
+    public void Motion(float speed);
     {
-        Carrrr.speed += Convert.ToSingle(Console.ReadLine());
+        Carrrr.speed = Convert.ToSingle(Console.ReadLine());
+        if(Carrrr.speed > 0)
+        {
+            Carrrr.state = "moving";
+        }
+        if(Carrrr.speed == 0)
+        {
+            Carrrr.state = "stationary";
+        }
+        if(Carrrr.speed < 0)
+        {
+            Console.WriteLine("That's not how reverse works..."
+        }
     }
 }
 }
